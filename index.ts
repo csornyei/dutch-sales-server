80fb5b38c7296c9c2cbd56d9907e1dace9ab7156
Mate Mark Csornyei <mate@csornyei.com>
2022-07-26 12:55:41 +0200

    loggin added

--- index.ts
+++ index.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import morgan from "morgan";
 import { getJumboSales } from "./jumbo";
 import db from "./database";
+import { createWriteStream } from "fs";
 
 const { PORT } = process.env;
 
 const app = express();
+
+app.use(morgan("short"));
+app.use(
+  morgan("short", {
+    stream: createWriteStream("./access.log", { flags: "a" }),
+  })
+);
 
 app.get("/", (_, res) => {
   res.send({
@@ -12,37 +21,41 @@ app.get("/", (_, res) => {
   });
 });
 
-app.get("/jumbo", async (req, res) => {
-  const results = await getJumboSales();
+app.get("/jumbo", async (_, res) => {
+  try {
+    const results = await getJumboSales();
 
-  const saveOps = [];
+    const saveOps = [];
 
-  for (const key in results) {
-    if (Object.prototype.hasOwnProperty.call(results, key)) {
-      const saleList = results[key];
-      for (const saleKey in saleList) {
-        if (Object.prototype.hasOwnProperty.call(saleList, saleKey)) {
-          const sale = saleList[saleKey];
-          saveOps.push(
-            new Promise(async (res, rej) => {
-              try {
-                const id = await db.save(sale);
-                res(id);
-              } catch (error) {
-                rej(error);
-              }
-            })
-          );
+    for (const key in results) {
+      if (Object.prototype.hasOwnProperty.call(results, key)) {
+        const saleList = results[key];
+        for (const saleKey in saleList) {
+          if (Object.prototype.hasOwnProperty.call(saleList, saleKey)) {
+            const sale = saleList[saleKey];
+            saveOps.push(
+              new Promise(async (res, rej) => {
+                try {
+                  const id = await db.save(sale);
+                  res(id);
+                } catch (error) {
+                  rej(error);
+                }
+              })
+            );
+          }
         }
       }
     }
+
+    const result = await Promise.all(saveOps);
+
+    res.send({
+      result,
+    });
+  } catch (error) {
+    res.status(500).send(error);
   }
-
-  const result = await Promise.all(saveOps);
-
-  res.send({
-    result,
-  });
 });
 
 app.listen(PORT, () => {
